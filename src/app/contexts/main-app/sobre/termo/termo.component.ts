ccf2751020f0ad1711c2ff2bd56ffba7216bcd80
juhachmann <juliana.hachmann@gmail.com>
2024-08-25 13:58:44 -0300

    feat: (doc) Adicionados Termo de Uso e Política de Privacidade

---
+++ src/app/contexts/main-app/sobre/termo/termo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-termo',
+  standalone: true,
+  imports: [],
+  templateUrl: './termo.component.html',
+  styleUrl: './termo.component.css'
+})
+export class TermoComponent {
+
+}
