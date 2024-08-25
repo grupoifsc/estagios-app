c928723368789140a2f6a224011daf8414f6ed82
juhachmann <juliana.hachmann@gmail.com>
2024-08-25 13:55:58 -0300

    feat: (doc) Adicionados Termo de Uso e Política de Privacidade à seção Sobre
    Texto e formatação HTML gerados com auxílio do ChatGPT para que ficasse conforme as práticas comuns deste tipo de documentação

--- src/app/contexts/main-app/sobre/sobre.component.ts
+++ src/app/contexts/main-app/sobre/sobre.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TabViewModule } from 'primeng/tabview';
+import { PrivacidadeComponent } from './privacidade/privacidade.component';
+import { TermoComponent } from './termo/termo.component';
 
 @Component({
   selector: 'app-sobre',
   standalone: true,
-  imports: [TabViewModule],
+  imports: [TabViewModule, PrivacidadeComponent, TermoComponent, ],
   templateUrl: './sobre.component.html',
   styleUrl: './sobre.component.css'
 })
